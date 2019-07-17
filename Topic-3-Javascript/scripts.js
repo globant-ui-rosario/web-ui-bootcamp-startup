ca19287ca4f4636bae03c02e0a8cbb8f16beb2eb
Marietta Bellunghi <mariettabellunghi@gmail.com>
2019-07-17 19:09:21 -0300

    alert added

--- Topic-3-Javascript/scripts.js
+++ Topic-3-Javascript/scripts.js
@@ -1,15 +1,9 @@
-const sayHowdyBtn = document.querySelector('button[name="henlo"]');
+const sayHowdyBtn = document.querySelector('button');
 
-function initial(event) {
-
-  const sectionEl = document.querySelector('section');
-  
-}
-
- function sayHenlo() {
+ sayHowdyBtn.addEventListener('click', function sayHenlo(){
   alert('Hello!');
-}
+ 
+});
+  
 
-window.addEventListener('load', initial);
 
- sayHowdyBtn.addEventListener('click', sayHenlo);
