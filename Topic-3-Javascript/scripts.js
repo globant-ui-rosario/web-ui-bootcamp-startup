16530f0f80aa2abf1c36a893174fd140415b1267
Marietta Bellunghi <mariettabellunghi@gmail.com>
2019-07-21 12:31:37 -0300

    jokes added

--- Topic-3-Javascript/scripts.js
+++ Topic-3-Javascript/scripts.js
@@ -1,9 +1,24 @@
-const sayHowdyBtn = document.querySelector('button');
+const sectionEl = document.querySelector('section');
+const sayHowdyBtn = document.querySelector('button[name="henlo"]');
+const getCNJokeBtn = document.querySelector('button[name="joke"]');
 
  sayHowdyBtn.addEventListener('click', function sayHenlo(){
   alert('Hello!');
  
 });
   
+
+
+function getCNJoke() {
+  const request = new XMLHttpRequest();
+  request.open('get', 'http://api.icndb.com/jokes/random', true);
+  request.responseType = 'json';
+  request.onload = function() {
+    sectionEl.querySelector('h1').innerHTML = request.response.value.joke;
+  };
+  request.send();
+}
+
+ getCNJokeBtn.addEventListener('click', getCNJoke);
 
 
